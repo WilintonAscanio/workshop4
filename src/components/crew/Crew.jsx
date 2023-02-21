75b85694070932e3a58ddadd07f44f3b415556fb
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-02-21 15:17:49 -0500

    Add branch develop

--- src/components/crew/Crew.jsx
+++ src/components/crew/Crew.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { NavLink, Outlet } from 'react-router-dom';
 import { getCrew } from '../../services/getCrew';
+import "./crew.scss"
 
 const Crew = () => {
   const [crew, setCrew] = useState([])
