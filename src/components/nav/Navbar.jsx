a7dd8db8f567200ce32dfbdc6db9cb07b032bd39
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-02-22 22:41:49 -0500

    Add menu responsive

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import "./nav.scss"
 import icon from "../../assets/shared/logo.svg"
+import iconHamburguer from "../../assets/shared/icon-hamburger.svg"
+import iconClose from "../../assets/shared/icon-close.svg"
 
 const Navbar = () => {
+    const [hamburguer, setHamburguer] = useState('hidden')
+    const [close, setClose] = useState('closeMenu')
+    const [open, setOpen] = useState('')
+
+    const openMenu = (icon) => {
+        if (icon === "hamburguer") {
+            setHamburguer('closeMenu')
+            setClose('hidden')
+            setOpen('open')
+
+        }
+        else {
+            setHamburguer('hidden')
+            setClose('closeMenu')
+            setOpen('')
+        }
+
+    }
+
+
     return (
         <header className='header'>
             <figure>
                 <img src={icon} alt="icon" />
             </figure>
             <small></small>
-            <nav className='header__navBar'>
+            <nav className={`header__navBar ${open}`}>
                 <ul>
+                    <img src={iconClose} alt="cerrar" className={close} onClick={() => openMenu("close")} />
+
                     <NavLink to='/' className='header__navBar__link'><span>00</span> HOME </NavLink>
                     <NavLink to='destination/Moon' className='header__navBar__link'><span>01</span> DESTINATION </NavLink>
                     <NavLink to='crew' className='header__navBar__link'><span>02</span> CREW </NavLink>
@@ -19,6 +43,7 @@ const Navbar = () => {
 
                 </ul>
             </nav>
+            <img src={iconHamburguer} alt="hamburguer" className={hamburguer} onClick={() => openMenu("hamburguer")} />
 
         </header>
 
